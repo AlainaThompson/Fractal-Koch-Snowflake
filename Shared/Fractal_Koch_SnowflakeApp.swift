eb0fe40d54500074a2d1b141497dd662b2285e01
Alaina Thompson <athompson9@hawk.iit.edu>
2022-02-04 11:58:56 -0600

    Initial Commit

---
+++ Shared/Fractal_Koch_SnowflakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fractal_Koch_SnowflakeApp.swift
+//  Shared
+//
+//  Created by Alaina Thompson on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Fractal_Koch_SnowflakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
